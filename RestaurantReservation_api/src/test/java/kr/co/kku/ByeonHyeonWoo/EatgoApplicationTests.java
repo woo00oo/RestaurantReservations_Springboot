ba414cc8f01ea2729f932dec93f176b8e5d17724
woo00oo <gusdn1018!@>
2020-09-16 16:20:27 +0900

    SpringBootSetUp

---
+++ RestaurantReservation_api/src/test/java/kr/co/kku/ByeonHyeonWoo/EatgoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kku.ByeonHyeonWoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
