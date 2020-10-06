b7b8357e6216f2b5e4c9655ee1ff93fa6e03e8ee
woo00oo <96woo94@naver.com>
2020-10-06 23:19:26 +0900

    메뉴 추가 api 구현 1

---
+++ RestaurantReservation_api/src/test/java/kr/co/kku/ByeonHyeonWoo/interfaces/MenuItemControllerTests.java
@@ -0,0 +1,41 @@
+package kr.co.kku.ByeonHyeonWoo.interfaces;
+
+import kr.co.kku.ByeonHyeonWoo.application.MenuItemService;
+import kr.co.kku.ByeonHyeonWoo.domain.MenuItem;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MenuItemController.class)
+public class MenuItemControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private MenuItemService menuItemService;
+
+    @Test
+    public void bulkUpdate() throws Exception {
+        mvc.perform(patch("/restaurants/1/menuitems")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("[]"))
+                .andExpect(status().isOk());
+
+        verify(menuItemService).bulkUpdate(eq(1L),any());
+    }
+
+}
