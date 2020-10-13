a8b744a17e66f798e4efc66f5560f0c80f1c1940
woo00oo <96woo94@naver.com>
2020-10-13 09:57:12 +0900

    customer api 수정

--- RestaurantReservation_customer_api/src/main/java/kr/co/kku/ByeonHyeonWoo/interfaces/RestaurantController.java
+++ RestaurantReservation_customer_api/src/main/java/kr/co/kku/ByeonHyeonWoo/interfaces/RestaurantController.java
@@ -33,30 +33,4 @@ public class RestaurantController {
         return restaurant;
     }
 
-    //가게 삽입
-    @PostMapping("/restaurants")
-    public ResponseEntity<?> create(@Valid @RequestBody Restaurant resource)
-            throws URISyntaxException {
-        Restaurant restaurant = restaurantService.addRestaurant(
-                Restaurant.builder()
-                .name(resource.getName())
-                .address(resource.getAddress())
-                .build());
-
-        URI location = new URI("/restaurants/" +restaurant.getId());
-        return ResponseEntity.created(location).body("{}");
-    }
-
-    //가게 수정
-    @PatchMapping("/restaurants/{id}")
-    public String update(@PathVariable("id") Long id,
-                         @Valid @RequestBody Restaurant resource){
-        String name = resource.getName();
-        String address = resource.getAddress();
-        restaurantService.updateRestaurant(id,name,address);
-
-        return "{}";
-    }
-
-
 }
