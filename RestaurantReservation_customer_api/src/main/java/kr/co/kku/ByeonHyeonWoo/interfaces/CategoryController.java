2a07f45e5890363c4037f98c80320919d6200d68
woo00oo <96woo94@naver.com>
2020-10-17 19:48:36 +0900

    가게 목록 필터링 구현 (카테고리별) - CustomerApi 적용

---
+++ RestaurantReservation_customer_api/src/main/java/kr/co/kku/ByeonHyeonWoo/interfaces/CategoryController.java
@@ -0,0 +1,29 @@
+package kr.co.kku.ByeonHyeonWoo.interfaces;
+
+import kr.co.kku.ByeonHyeonWoo.application.CategoryService;
+import kr.co.kku.ByeonHyeonWoo.domain.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/categories")
+    public List<Category> list(){
+        List<Category> categories = categoryService.getCategories();
+        return categories;
+    }
+
+
+}
